b542890a7f2d8168f54d41074ef27e1fb6ed04ca
NicoBarbato <nico_barbato@hotmail.com>
2024-02-26 22:02:35 -0300

    final!

--- test_validacion_contrasenia.py
+++ test_validacion_contrasenia.py
@@ -4,12 +4,12 @@ from validacion_contrasenia import valid_form
 class testValidForm(unittest.TestCase):
     def test_valid_form(self):
         result = valid_form(
-            username = "prueba"
+            username = "prueba",
             password = "pruebacontra"
         )
         self.assertTrue(result)
         result = valid_form(
-            username = "prueba"
+            username = "prueba",
             password = "pruebacontra"
         )
         self.assertFalse(result)
